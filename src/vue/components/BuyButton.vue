8202eaf5dae11ae9b3feaced7cfd2c3587c0d3bb
Casper Panduro <cap@checkmate.dk>
2022-04-22 23:33:22 +0200

    add to cart functionality

---
+++ src/vue/components/BuyButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <form method="post" action="/cart/add">
+    <input type="hidden" name="id" :value="productId" />
+    <input type="hidden" id="quantity" name="quantity" value="1"/>
+    <button type="submit" class="px-6 py-2 text-[12px] md:px-10 md:py-2 sm:text-sm mx-auto text-white bg-black rounded-full inline-flex gap-2 items-center justify-center">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+      </svg>
+      <span>Buy</span>
+    </button>
+  </form>
+</template>
+<script>
+export default {
+  props: {
+    productId: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
---
+++ src/vue/components/PanelSlide.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="fixed inset-0 bg-black opacity-20"></div>
+    <div class="fixed top-0 right-0 bottom-0 bg-white w-full sm:w-[450px] flex items-stretch">
+      <div class="absolute top-8 right-8">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: 'PanelSlide',
+}
+</script>
